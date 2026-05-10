45a1fdb344c3a2800f04853a6cd7f78a06820881
JedEatSnax <134795258+JedEatSnax@users.noreply.github.com>
2026-05-11 05:38:17 +0800

    chore(client): implement modal boilerplate for org

--- client/src/pages/Organizations.tsx
+++ client/src/pages/Organizations.tsx
@@ -2,6 +2,20 @@ import { AppSidebar } from "@/components/app-sidebar"
 import { SiteHeader } from "@/components/site-header"
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
 import { type CSSProperties } from "react"
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Field, FieldGroup } from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 
 export default function Organizations() {
   return (
@@ -18,7 +32,60 @@ export default function Organizations() {
         <SiteHeader title="Organizations" />
         <div className="flex flex-1 flex-col">
           <div className="@container/main flex flex-1 flex-col gap-2">
-            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6"></div>
+            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+              <Dialog>
+                <form>
+                  <DialogTrigger asChild>
+                    <Button variant="outline">Create Organization</Button>
+                  </DialogTrigger>
+                  <DialogContent className="sm:max-w-sm">
+                    <DialogHeader>
+                      <DialogTitle>Create Organization</DialogTitle>
+                      <DialogDescription>
+                        Make changes to your profile here. Click save when
+                        you&apos;re done.
+                      </DialogDescription>
+                    </DialogHeader>
+                    <FieldGroup>
+                      <Field>
+                        <Label htmlFor="name-1">
+                          Membership Service Provider ID
+                        </Label>
+                        <Input
+                          id="name-1"
+                          name="name"
+                          defaultValue="Pedro Duarte"
+                        />
+                      </Field>
+                      <Field>
+                        <Label htmlFor="username-1">Username</Label>
+                        <Input
+                          id="username-1"
+                          name="username"
+                          defaultValue="@peduarte"
+                        />
+                      </Field>
+                      <Field>
+                        <Label htmlFor="password-1">Password</Label>
+                        <Input
+                          id="password-1"
+                          name="username"
+                          defaultValue="@peduarte"
+                        />
+                      </Field>
+                    </FieldGroup>
+                    <DialogFooter>
+                      <DialogClose asChild>
+                        <Button variant="destructive" className="sm:mr-auto">
+                          Cancel
+                        </Button>
+                      </DialogClose>
+                      <Button type="submit">Save changes</Button>
+                    </DialogFooter>
+                  </DialogContent>
+                </form>
+              </Dialog>
+            </div>
           </div>
         </div>
       </SidebarInset>
