f7322b06cb962ec860944ed4b3ece703ad1bf3d6
Wiii-1 <markjunemarino@gmail.con>
2026-07-10 23:59:07 +0800

    hard reset

--- backend/db/db.js
+++
@@ -1,7 +0,0 @@
-const knex = require('knex');
-const knexfile = require('./knexfile');
-
-const env = process.env.NODE_ENV || 'development';
-const config = knexfile[env] || knexfile.development;
-
-module.exports = knex(config);
--- backend/vault/policies/policies.hcl
+++
@@ -1,7 +0,0 @@
-path "fabric/*" {
-  capabilities = ["create", "read", "update", "delete", "list"]
-}
-
-path "sys/health" {
-  capabilities = ["read"]
-}
